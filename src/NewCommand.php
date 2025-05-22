eba8dc57a494bfd902d1fdab5a1b155797a4e81d
Mathieu <mathieu@fellicht.nl>
2025-05-22 15:19:29 +0200

    init

---
+++ src/NewCommand.php
@@ -0,0 +1,591 @@
+<?php
+
+namespace Backstage\Installer;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
+use Illuminate\Support\ProcessUtils;
+use Illuminate\Support\Str;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\PhpExecutableFinder;
+use Symfony\Component\Process\Process;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\select;
+use function Laravel\Prompts\text;
+
+class NewCommand extends Command
+{
+    use Concerns\ConfiguresPrompts;
+    use Concerns\InteractsWithHerdOrValet;
+
+    /**
+     * The Composer instance.
+     *
+     * @var \Illuminate\Support\Composer
+     */
+    protected $composer;
+
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Create a new stage')
+            ->addArgument('name', InputArgument::REQUIRED)
+            ->addOption('database', null, InputOption::VALUE_REQUIRED, 'The database driver your application will use')
+            ->addOption('force', 'f', InputOption::VALUE_NONE, 'Forces install even if the directory already exists');
+    }
+
+    /**
+     * Interact with the user before validating the input.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        parent::interact($input, $output);
+
+        $this->configurePrompts($input, $output);
+
+        $output->write(PHP_EOL.'  <fg=magenta>  _____ _                  
+  / ____| | Welcome on                 
+ | (___ | |_ __ _  __ _  ___ 
+  \___ \| __/ _` |/ _` |/ _ \
+  ____) | || (_| | (_| |  __/
+ |_____/ \__\__,_|\__, |\___|
+                   __/ |     
+                  |___/      
+</>'.PHP_EOL.PHP_EOL);
+
+        $this->ensureExtensionsAreAvailable($input, $output);
+
+        if (! $input->getArgument('name')) {
+            $input->setArgument('name', text(
+                label: 'What is the name of the show?',
+                placeholder: 'E.g. disco-fever',
+                required: 'The showname is required.',
+                validate: function ($value) use ($input) {
+                    if (preg_match('/[^\pL\pN\-_.]/', $value) !== 0) {
+                        return 'The name may only contain letters, numbers, dashes, underscores, and periods.';
+                    }
+
+                    if ($input->getOption('force') !== true) {
+                        try {
+                            $this->verifyApplicationDoesntExist($this->getInstallationDirectory($value));
+                        } catch (RuntimeException $e) {
+                            return 'Show already exists.';
+                        }
+                    }
+                },
+            ));
+        }
+
+        if ($input->getOption('force') !== true) {
+            $this->verifyApplicationDoesntExist(
+                $this->getInstallationDirectory($input->getArgument('name'))
+            );
+        }
+    }
+
+    /**
+     * Ensure that the required PHP extensions are installed.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
+    protected function ensureExtensionsAreAvailable(InputInterface $input, OutputInterface $output): void
+    {
+        $availableExtensions = get_loaded_extensions();
+
+        $missingExtensions = collect([
+            'ctype',
+            'filter',
+            'hash',
+            'mbstring',
+            'openssl',
+            'session',
+            'tokenizer',
+        ])->reject(fn ($extension) => in_array($extension, $availableExtensions));
+
+        if ($missingExtensions->isEmpty()) {
+            return;
+        }
+
+        throw new \RuntimeException(
+            sprintf('The following PHP extensions are required but are not installed: %s', $missingExtensions->join(', ', ', and '))
+        );
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->validateDatabaseOption($input);
+
+        $name = rtrim($input->getArgument('name'), '/\\');
+
+        $directory = $this->getInstallationDirectory($name);
+
+        $this->composer = new Composer(new Filesystem(), $directory);
+
+        if (! $input->getOption('force')) {
+            $this->verifyApplicationDoesntExist($directory);
+        }
+
+        if ($input->getOption('force') && $directory === '.') {
+            throw new RuntimeException('Cannot use --force option when using current directory for installation!');
+        }
+
+        $composer = $this->findComposer();
+        $phpBinary = $this->phpBinary();
+
+        $createProjectCommand = $composer." create-project backstage/site \"$directory\" --remove-vcs --prefer-dist --no-scripts --repository='{\"url\": \"git@github.com:backstagephp/site.git\", \"type\": \"vcs\"}' --stability=dev";
+
+        $commands = [
+            $createProjectCommand,
+            $composer." run post-root-package-install -d \"$directory\"",
+            $phpBinary." \"$directory/artisan\" key:generate --ansi",
+        ];
+
+        if ($directory != '.' && $input->getOption('force')) {
+            if (PHP_OS_FAMILY == 'Windows') {
+                array_unshift($commands, "(if exist \"$directory\" rd /s /q \"$directory\")");
+            } else {
+                array_unshift($commands, "rm -rf \"$directory\"");
+            }
+        }
+
+        if (PHP_OS_FAMILY != 'Windows') {
+            $commands[] = "chmod 755 \"$directory/artisan\"";
+        }
+
+        if (($process = $this->runCommands($commands, $input, $output))->isSuccessful()) {
+            if ($name !== '.') {
+                $this->replaceInFile(
+                    'APP_URL=http://localhost',
+                    'APP_URL='.$this->generateAppUrl($name, $directory),
+                    $directory.'/.env'
+                );
+
+                [$database, $migrate] = $this->promptForDatabaseOptions($directory, $input);
+
+                $this->configureDefaultDatabaseConnection($directory, $database, $name);
+
+                if ($migrate) {
+
+                    $commands = [
+                        trim(sprintf(
+                            $this->phpBinary().' artisan migrate %s',
+                            ! $input->isInteractive() ? '--no-interaction' : '',
+                        )),
+                    ];
+
+                    $this->runCommands($commands, $input, $output, workingPath: $directory);
+                }
+            }
+
+            $this->configureComposerDevScript($directory);
+
+            $this->runCommands(['npm install', 'npm run build'], $input, $output, workingPath: $directory);
+
+            $output->writeln("  <bg=magenta;fg=white> INFO </> The stage <options=bold>[{$name}]</> is yours. You can start your local development using:".PHP_EOL);
+            $output->writeln('<fg=gray>➜</> <options=bold>cd '.$name.'</>');
+
+            if ($this->isParkedOnHerdOrValet($directory)) {
+                $url = $this->generateAppUrl($name, $directory);
+                $output->writeln('<fg=gray>➜</> Open: <options=bold;href='.$url.'>'.$url.'</>');
+            } else {
+                $output->writeln('<fg=gray>➜</> <options=bold>composer run dev</>');
+            }
+
+            $output->writeln('');
+            $output->writeln('New to <fg=magenta>Backstage</>? Check the <href=https://docs.backstagephp.com/quick-start.html>script</> and <options=bold>enjoy the performance!</>');
+            $output->writeln('');
+        }
+
+        return $process->getExitCode();
+    }
+
+    /**
+     * Configure the default database connection.
+     *
+     * @param  string  $directory
+     * @param  string  $database
+     * @param  string  $name
+     * @return void
+     */
+    protected function configureDefaultDatabaseConnection(string $directory, string $database, string $name)
+    {
+        $this->pregReplaceInFile(
+            '/DB_CONNECTION=.*/',
+            'DB_CONNECTION='.$database,
+            $directory.'/.env'
+        );
+
+        $this->pregReplaceInFile(
+            '/DB_CONNECTION=.*/',
+            'DB_CONNECTION='.$database,
+            $directory.'/.env.example'
+        );
+
+        $this->uncommentDatabaseConfiguration($directory);
+
+        $defaultPorts = [
+            'pgsql' => '5432',
+            'sqlsrv' => '1433',
+        ];
+
+        if (isset($defaultPorts[$database])) {
+            $this->replaceInFile(
+                'DB_PORT=3306',
+                'DB_PORT='.$defaultPorts[$database],
+                $directory.'/.env'
+            );
+
+            $this->replaceInFile(
+                'DB_PORT=3306',
+                'DB_PORT='.$defaultPorts[$database],
+                $directory.'/.env.example'
+            );
+        }
+
+        $this->replaceInFile(
+            'DB_DATABASE=laravel',
+            'DB_DATABASE='.str_replace('-', '_', strtolower($name)),
+            $directory.'/.env'
+        );
+
+        $this->replaceInFile(
+            'DB_DATABASE=laravel',
+            'DB_DATABASE='.str_replace('-', '_', strtolower($name)),
+            $directory.'/.env.example'
+        );
+    }
+
+    /**
+     * Uncomment the relevant database configuration entries for non SQLite applications.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function uncommentDatabaseConfiguration(string $directory)
+    {
+        $defaults = [
+            '# DB_HOST=127.0.0.1',
+            '# DB_PORT=3306',
+            '# DB_DATABASE=laravel',
+            '# DB_USERNAME=root',
+            '# DB_PASSWORD=',
+        ];
+
+        $this->replaceInFile(
+            $defaults,
+            collect($defaults)->map(fn ($default) => substr($default, 2))->all(),
+            $directory.'/.env'
+        );
+
+        $this->replaceInFile(
+            $defaults,
+            collect($defaults)->map(fn ($default) => substr($default, 2))->all(),
+            $directory.'/.env.example'
+        );
+    }
+
+    /**
+     * Determine the default database connection.
+     *
+     * @param  string  $directory
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @return array
+     */
+    protected function promptForDatabaseOptions(string $directory, InputInterface $input)
+    {
+        $defaultDatabase = collect(
+            $databaseOptions = $this->databaseOptions()
+        )->keys()->first();
+
+        if (! $input->getOption('database') && $input->isInteractive()) {
+            $input->setOption('database', select(
+                label: 'Which database will your application use?',
+                options: $databaseOptions,
+                default: $defaultDatabase,
+            ));
+
+            $migrate = confirm(
+                label: 'Default database updated. Would you like to run the default database migrations?'
+            );
+        }
+
+        return [$input->getOption('database') ?? $defaultDatabase, $migrate ?? $input->hasOption('database')];
+    }
+
+    /**
+     * Get the available database options.
+     *
+     * @return array
+     */
+    protected function databaseOptions(): array
+    {
+        return collect([
+            'mysql' => ['MySQL', extension_loaded('pdo_mysql')],
+            'mariadb' => ['MariaDB', extension_loaded('pdo_mysql')],
+            'pgsql' => ['PostgreSQL', extension_loaded('pdo_pgsql')],
+            'sqlsrv' => ['SQL Server', extension_loaded('pdo_sqlsrv')],
+        ])
+            ->sortBy(fn ($database) => $database[1] ? 0 : 1)
+            ->map(fn ($database) => $database[0].($database[1] ? '' : ' (Missing PDO extension)'))
+            ->all();
+    }
+
+    /**
+     * Validate the database driver input.
+     *
+     * @param  \Symfony\Components\Console\Input\InputInterface  $input
+     */
+    protected function validateDatabaseOption(InputInterface $input)
+    {
+        if ($input->getOption('database') && ! in_array($input->getOption('database'), $drivers = ['mysql', 'mariadb', 'pgsql', 'sqlsrv'])) {
+            throw new \InvalidArgumentException("Invalid database driver [{$input->getOption('database')}]. Valid options are: ".implode(', ', $drivers).'.');
+        }
+    }
+
+    /**
+     * Configure the Composer "dev" script.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function configureComposerDevScript(string $directory): void
+    {
+        $this->composer->modify(function (array $content) {
+            if (windows_os()) {
+                $content['scripts']['dev'] = [
+                    'Composer\\Config::disableProcessTimeout',
+                    "npx concurrently -c \"#93c5fd,#c4b5fd,#fdba74\" \"php artisan serve\" \"php artisan queue:listen --tries=1\" \"npm run dev\" --names='server,queue,vite'",
+                ];
+            }
+
+            return $content;
+        });
+    }
+
+    /**
+     * Verify that the application does not already exist.
+     *
+     * @param  string  $directory
+     * @return void
+     */
+    protected function verifyApplicationDoesntExist($directory)
+    {
+        if ((is_dir($directory) || is_file($directory)) && $directory != getcwd()) {
+            throw new RuntimeException('Application already exists!');
+        }
+    }
+
+    /**
+     * Generate a valid APP_URL for the given application name.
+     *
+     * @param  string  $name
+     * @param  string  $directory
+     * @return string
+     */
+    protected function generateAppUrl($name, $directory)
+    {
+        if (! $this->isParkedOnHerdOrValet($directory)) {
+            return 'http://localhost:8000';
+        }
+
+        $hostname = mb_strtolower($name).'.'.$this->getTld();
+
+        return $this->canResolveHostname($hostname) ? 'http://'.$hostname : 'http://localhost';
+    }
+
+    /**
+     * Get the TLD for the application.
+     *
+     * @return string
+     */
+    protected function getTld()
+    {
+        return $this->runOnValetOrHerd('tld') ?: 'test';
+    }
+
+    /**
+     * Determine whether the given hostname is resolvable.
+     *
+     * @param  string  $hostname
+     * @return bool
+     */
+    protected function canResolveHostname($hostname)
+    {
+        return gethostbyname($hostname.'.') !== $hostname.'.';
+    }
+
+    /**
+     * Get the installation directory.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function getInstallationDirectory(string $name)
+    {
+        return $name !== '.' ? getcwd().'/'.$name : '.';
+    }
+
+    /**
+     * Get the composer command for the environment.
+     *
+     * @return string
+     */
+    protected function findComposer()
+    {
+        return implode(' ', $this->composer->findComposer());
+    }
+
+    /**
+     * Get the path to the appropriate PHP binary.
+     *
+     * @return string
+     */
+    protected function phpBinary()
+    {
+        $phpBinary = function_exists('Illuminate\Support\php_binary')
+            ? \Illuminate\Support\php_binary()
+            : (new PhpExecutableFinder)->find(false);
+
+        return $phpBinary !== false
+            ? ProcessUtils::escapeArgument($phpBinary)
+            : 'php';
+    }
+
+    /**
+     * Run the given commands.
+     *
+     * @param  array  $commands
+     * @param  \Symfony\Component\Console\Input\InputInterface  $input
+     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param  string|null  $workingPath
+     * @param  array  $env
+     * @return \Symfony\Component\Process\Process
+     */
+    protected function runCommands($commands, InputInterface $input, OutputInterface $output, ?string $workingPath = null, array $env = [])
+    {
+        if (! $output->isDecorated()) {
+            $commands = array_map(function ($value) {
+                if (Str::startsWith($value, ['chmod', 'git', $this->phpBinary().' ./vendor/bin/pest'])) {
+                    return $value;
+                }
+
+                return $value.' --no-ansi';
+            }, $commands);
+        }
+
+        if ($input->getOption('quiet')) {
+            $commands = array_map(function ($value) {
+                if (Str::startsWith($value, ['chmod', 'git', $this->phpBinary().' ./vendor/bin/pest'])) {
+                    return $value;
+                }
+
+                return $value.' --quiet';
+            }, $commands);
+        }
+
+        $process = Process::fromShellCommandline(implode(' && ', $commands), $workingPath, $env, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $output->writeln('  <bg=yellow;fg=black> WARN </> '.$e->getMessage().PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) use ($output) {
+            $output->write('    '.$line);
+        });
+
+        return $process;
+    }
+
+    /**
+     * Replace the given file.
+     *
+     * @param  string  $replace
+     * @param  string  $file
+     * @return void
+     */
+    protected function replaceFile(string $replace, string $file)
+    {
+        $stubs = dirname(__DIR__).'/stubs';
+
+        file_put_contents(
+            $file,
+            file_get_contents("$stubs/$replace"),
+        );
+    }
+
+    /**
+     * Replace the given string in the given file.
+     *
+     * @param  string|array  $search
+     * @param  string|array  $replace
+     * @param  string  $file
+     * @return void
+     */
+    protected function replaceInFile(string|array $search, string|array $replace, string $file)
+    {
+        file_put_contents(
+            $file,
+            str_replace($search, $replace, file_get_contents($file))
+        );
+    }
+
+    /**
+     * Replace the given string in the given file using regular expressions.
+     *
+     * @param  string|array  $search
+     * @param  string|array  $replace
+     * @param  string  $file
+     * @return void
+     */
+    protected function pregReplaceInFile(string $pattern, string $replace, string $file)
+    {
+        file_put_contents(
+            $file,
+            preg_replace($pattern, $replace, file_get_contents($file))
+        );
+    }
+
+    /**
+     * Delete the given file.
+     *
+     * @param  string  $file
+     * @return void
+     */
+    protected function deleteFile(string $file)
+    {
+        unlink($file);
+    }
+}
